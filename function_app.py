41e0ee1dd037e8c537495ee79ecba55296d0732c
Warit Santaputra <waritsan@gmail.com>
2025-08-08 13:59:12 +0700

    change model

--- function_app.py
+++ function_app.py
@@ -40,7 +40,7 @@ def openai_assistant_http_trigger(req: func.HttpRequest) -> func.HttpResponse:
 
         # Create assistant
         assistant = client.beta.assistants.create(
-            model="gpt-35-turbo",  # replace with model deployment name.
+            model="gpt-4.1-nano",  # replace with model deployment name.
             instructions="Respond in the same language as the user's question.",
             tools=[{"type": "file_search"}],
             tool_resources={"file_search": {"vector_store_ids": ["vs_sgjvlbgxltrP9k58KcCsHGFw"]}},
